f12c577338ec4e922bb17780c172d71e6b46f15e
omrobbie <putu.roby@yahoo.com>
2020-10-24 15:40:22 +0800

    create orange rectangle with texture

--- TextureWorkshop/Session 1 - Basic/Start/BasicTextureViewController.swift
+++ TextureWorkshop/Session 1 - Basic/Start/BasicTextureViewController.swift
@@ -5,8 +5,25 @@
 //  Created by Jefferson Setiawan on 06/10/20.
 //
 
-import UIKit
+import AsyncDisplayKit
 
-class BasicTextureViewController: UIViewController {
-   
+class BasicTextureViewController: ASDKViewController<ASDisplayNode> {
+    override init() {
+        super.init(node: ASDisplayNode())
+        node.backgroundColor = .white
+
+        let subNode = ASDisplayNode()
+        subNode.backgroundColor = .orange
+        subNode.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+
+        node.addSubnode(subNode)
+
+        node.layoutSpecBlock = { (_, _) -> ASLayoutSpec in
+            return ASCenterLayoutSpec(centeringOptions: .XY, sizingOptions: [], child: subNode)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
